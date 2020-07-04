d0c1dc1086db44f6a2f80de9b9356b95a8a8effd
karishma3199 <kanu.karishma3199@gmail.com>
2020-07-04 18:25:07 +0530

    Updates

---
+++ precache-manifest.a64d1e94d2de04e1497b08295ace71d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd733e2ef6ced97f500dc5234a76151",
+    "url": "/rpf/index.html"
+  },
+  {
+    "revision": "cd32554e53068e3088fa",
+    "url": "/rpf/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8dd6bf642d69a8d4e20b",
+    "url": "/rpf/static/css/main.3c7f8857.chunk.css"
+  },
+  {
+    "revision": "cd32554e53068e3088fa",
+    "url": "/rpf/static/js/2.7b1b664b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rpf/static/js/2.7b1b664b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd6bf642d69a8d4e20b",
+    "url": "/rpf/static/js/main.e73a680e.chunk.js"
+  },
+  {
+    "revision": "5d8a8a8df016eea95c50",
+    "url": "/rpf/static/js/runtime-main.c525fdb5.js"
+  }
+]);
